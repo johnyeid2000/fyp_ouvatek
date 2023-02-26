1543637668308c4623c3a892cdcbafe50d097875
johnyeid2000 <johny_eid@live.com>
2023-02-26 10:53:44 +0200

    commit reverting last commit :)

--- Server/connectdb.js
+++ Server/connectdb.js
@@ -1,27 +1,26 @@
 var mysql = require('mysql');
 const connection = mysql.createConnection({
-	host:"sql112.epizy.com",
-	user:"epiz_26693603",
-	password:"Y7ipe4V1EUuXk",
-	database : "epiz_26693603_ouvatek",
+	host:"localhost",
+	user:"root",
+	password:"",
+	database : "ouvatek",
 })
-connection.connect();
 
-// // Connecting to database
-// connection.connect(function(err) {
-// 	if(err){
-// 	console.log("Error in the connection")
-// 	console.log(err)
-// 	}
-// 	else{
-// 	console.log(`Database Connected`)
-// 	connection.query(`SHOW DATABASES`,
-// 	function (err, result) {
-// 		if(err)
-// 		console.log(`Error executing the query - ${err}`)
-// 		else
-// 		console.log("Result: ",result)
-// 	})
-// 	}
-// })
+// Connecting to database
+connection.connect(function(err) {
+	if(err){
+	console.log("Error in the connection")
+	console.log(err)
+	}
+	else{
+	console.log(`Database Connected`)
+	connection.query(`SHOW DATABASES`,
+	function (err, result) {
+		if(err)
+		console.log(`Error executing the query - ${err}`)
+		else
+		console.log("Result: ",result)
+	})
+	}
+})
 exports.connection = connection;
