c7dd2e4787737f76c23149f028c7bc168875373b
elionoreamine <elionore.amine@hotmail.com>
2023-03-01 16:09:29 +0200

    extra changes

---
+++ FYPFront/src/components/CustomPicker/styles.js
@@ -0,0 +1,39 @@
+import {StyleSheet} from 'react-native';
+
+
+const styles =StyleSheet.create({
+    root:{
+        width:'100%',
+    },
+    container:{
+        backgroundColor:'white',
+        width:'100%',
+        
+        borderColor:'#e8e8e8',
+        borderWidth:1,
+        borderRadius:5,
+
+        paddingLeft:10,
+        marginVertical:5,
+
+        flexDirection: 'row',
+        alignItems:'center'
+    },
+    label:{
+        marginVertical:2,
+        fontWeight:'800',
+    },
+    icon:{
+        fontSize: 22, 
+    },
+    picker: {
+        width: 200,
+        color:'#a9a9a9',
+        flex:1
+    },
+  pickerItem:{
+    fontSize:14,
+  }
+});
+
+export default styles;
