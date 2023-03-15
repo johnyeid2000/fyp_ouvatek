0391ff1d735b187d3e429a40255025b87f131a28
johnyeid2000 <johny_eid@live.com>
2023-03-15 20:28:18 +0200

    connectdb

--- Server/connectdb.js
+++ Server/connectdb.js
@@ -1,9 +1,11 @@
 var mysql = require('mysql');
-const connection = mysql.createConnection({
+const connection = mysql.createPool({
 	host:"eu-cdbr-west-03.cleardb.net",
 	user:"b7deccfcfa1709",
 	password:"bb7ff2a9",
 	database : "heroku_e89ea0ca17612a0", 
+	connectionLimit: 999,
+	waitForConnections: true
 })
 
 // Connecting to database
