c65165aaf8b44fd667a304d8d274511487294a20
johnyeid2000 <johny_eid@live.com>
2023-03-15 20:33:37 +0200

    connect fix

--- Server/connectdb.js
+++ Server/connectdb.js
@@ -9,20 +9,11 @@ const connection = mysql.createPool({
 })
 
 // Connecting to database
-connection.connect(function(err) {
-	if(err){
-	console.log("Error in the connection")
-	console.log(err)
-	}
-	else{
-	console.log(`Database Connected`)
-	connection.query(`SHOW DATABASES`,
-	function (err, result) {
-		if(err)
+connection.query(`SHOW DATABASES`, function (err, result) {
+	if(err)
 		console.log(`Error executing the query - ${err}`)
-		else
+	else
 		console.log("Result: ",result)
-	})
-	}
-})
+	});
+
 exports.connection = connection;
