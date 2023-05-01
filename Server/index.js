abf55f4e117b6e1ac2b37c820eb15343751808d8
johnyeid2000 <johny_eid@live.com>
2023-05-01 14:36:16 +0300

    1 line removed

--- Server/index.js
+++ Server/index.js
@@ -10,7 +10,6 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT || 3000;
 const bodyParser = require("body-parser");
-const e = require("express");
 app.use(bodyParser.json({
 	type: "application/json"
 }));
